3aa7900a5a155e6714f1f5a4edcb51509b13199c
FelipeSousa366 <felipe36603466@gmail.com>
2022-05-19 08:53:00 -0300

    Projeto Agenda(Django)

---
+++ contatos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'mostrar')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    #list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar' )
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
---
+++ contatos/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
